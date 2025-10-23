6c9f5f2bd2d57604da12f6d187c48b1c32a7605b
V1lch1s <a01236306@tec.mx>
2025-10-23 14:24:23 -0600

    Landing v0.0.2

--- isolated-workshop/src/Landing.tsx
+++ isolated-workshop/src/Landing.tsx
@@ -18,9 +18,18 @@ function Landing() {
             height: "200px"
           }}
         /> */}
+
+
+        {/* Adornos */}
         <div
           aria-hidden="true"
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          className="fixed inset-x-0
+                     -left-50
+                     -top-40
+                     -z-0
+                     transform-gpu
+                     overflow-hidden
+                     blur-3xl"
         >
           <div
             style={{
@@ -28,9 +37,87 @@ function Landing() {
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
               background: "#fa8e9cff",
             }}
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-96 -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-289"
+            className="relative left-[calc(50%-11rem)]
+                       aspect-[1155/678]
+                       w-[36rem] -translate-x-1/2
+                       rotate-[30deg]
+                       bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]
+                       opacity-70"
           />
         </div>
+        <div
+          aria-hidden="true"
+          className="fixed inset-x-0
+                     -left-330
+                     top-50
+                     -z-0
+                     transform-gpu
+                     overflow-hidden
+                     blur-3xl"
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              background: "#fa8e9cff",
+            }}
+            className="relative left-[calc(50%-11rem)]
+                       aspect-[1155/678]
+                       w-[36rem] -translate-x-1/2
+                       rotate-[30deg]
+                       bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]
+                       opacity-70"
+          />
+        </div>
+        <div
+          aria-hidden="true"
+          className="fixed inset-x-0
+                     -right-0
+                     top-160
+                     -z-0 transform-gpu
+                     overflow-hidden
+                     blur-3xl"
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              background: "#fa8e9cff",
+            }}
+            className="relative left-[calc(50%+11rem)]
+                       aspect-[1155/678]
+                       w-[36rem] -translate-x-1/2
+                       rotate-[30deg]
+                       bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]
+                       opacity-70"
+          />
+        </div>
+        <div
+          aria-hidden="true"
+          className="fixed inset-x-0
+                     -right-270
+                     top-50
+                     -z-0 transform-gpu
+                     overflow-hidden
+                     blur-3xl"
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              background: "#fa8e9cff",
+            }}
+            className="relative left-[calc(50%+11rem)]
+                       aspect-[1155/678]
+                       w-[36rem] -translate-x-1/2
+                       rotate-[30deg]
+                       bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]
+                       opacity-70"
+          />
+        </div>
+
+
+        {/* Contenido de la vista */}
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">    
           <div className="justify-center items-center text-center">
             <div className="flex justify-center">
@@ -69,31 +156,6 @@ function Landing() {
               />
             </div>
           </div>
-        </div>
-        <div
-          aria-hidden="true"
-          className="absolute
-                     inset-x-0
-                     top-[calc(100%-13rem)]
-                     -z-10 transform-gpu
-                     overflow-hidden
-                     blur-3xl
-                     sm:top-[calc(100%-30rem)]"
-        >
-          <div
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              background: "#fa8e9cff",
-            }}
-            className="relative left-[calc(50%+3rem)]
-                       aspect-[1155/678]
-                       w-96 -translate-x-1/2
-                       bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]
-                       opacity-30
-                       sm:left-[calc(50%+36rem)]
-                       sm:w-289"
-          />
         </div>
       </div>
     </div>
